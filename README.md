1919b3d14949771f7c4bfeb31df9d3c9fa923b72
RiskierGriffin <tenenbaumallan@gmail.com>
2017-11-01 17:24:23 -0400

    Did some work, fixed Jacob's fuckups

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Rat-Race
+Rat Race by Allan Tenenbaum
+Mr.Goldman's class
--- Utilities.js
+++ Utilities.js
@@ -1,12 +1,10 @@
-// Memeking // 
-
-
+// Allan Tenenbaum
 function getRandomInteger(min, max) {
 	return Math.floor(Math.random() * (max-min + 1)) + min;
-} 
+}
 
 function getOrdinalSuffix(number){
-	
+
 	mod100 = number % 100;
 	mod10 = number % 10;
 	if (mod10 == 1 && mod100 != 11){
@@ -15,9 +13,9 @@ function getOrdinalSuffix(number){
 	if (mod10 == 2 && mod100 != 12){
 		return "nd";
 	}
-	if(mod10 = 3 && mod100 != 13){
+	if(mod10 == 3 && mod100 != 13){
 		return "rd";
 	} else{
 		return "th";
 	}
-} 
+}
